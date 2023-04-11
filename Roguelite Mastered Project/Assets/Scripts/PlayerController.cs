280a6cf204fb35aa213f275144579e800795652e
IShkorubsky <illyashkorubsky@hotmail.com>
2023-04-11 15:55:50 +0100

    - Organized scripts for future readability; - Solved bug with attacking towards mouse position;

--- Roguelite Mastered Project/Assets/Scripts/PlayerController.cs
+++ Roguelite Mastered Project/Assets/Scripts/PlayerController.cs
@@ -8,36 +8,40 @@ using UnityEngine.UI;
 //4 Done - Loose Health
 //5 Done - Handle Dying
 //6 Done - Handle Dodging
+//7 Done - Handle Attacking
 //------------------------------------------------------
 
 public class PlayerController : MonoBehaviour
 {
+    #region Variables
+
     [SerializeField] private PlayerStats playerStats;
-    [SerializeField] private DealDamage swordScript;
     public PlayerStats Stats => playerStats;
-    
+    [SerializeField] private DealDamage swordScript;
     [SerializeField] private Slider healthBar;
     
+    private Camera _myCamera;
     private Rigidbody _myRigidbody;
     private Animator _myAnimator;
+    
     private Vector3 _movementInput;
     private const float SmoothTime = 0.1f;
-    
+
     private bool _isDodging;
     private float _dodgeTimer;
     private float _attackTimer;
     private int _combo;
-    
+
     private static readonly int Running = Animator.StringToHash("Running");
     private static readonly int Dodging = Animator.StringToHash("Dodging");
     private static readonly int Combo = Animator.StringToHash("Combo");
 
-
-
+    #endregion
     private void Start()
     {
         _myRigidbody = gameObject.GetComponent<Rigidbody>();
         _myAnimator = gameObject.GetComponent<Animator>();
+        _myCamera = Camera.main;
         Stats.SetMaxHealth();
         _dodgeTimer = 1f;
         _attackTimer = 0.8f;
@@ -45,6 +49,8 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        #region Movement
+        
         if (!_isDodging)
         {
             if (Input.GetKeyDown(KeyCode.LeftShift))
@@ -57,21 +63,36 @@ public class PlayerController : MonoBehaviour
 
             Move();
         }
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Debug.Log("attacking");
-            StartCoroutine(PlayerAttack());
-        }
         
         if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
         {
-            _myAnimator.SetBool(Running,false);
+            _myAnimator.SetBool(Running, false);
             _myRigidbody.velocity = Vector3.zero;
         }
+        
+        #endregion
+
+        #region Attacking
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //Look towards mouse position
+            var ray = _myCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray,out RaycastHit raycastHit))
+            {
+                //Rotate player model towards hit point
+                transform.LookAt(new Vector3(raycastHit.point.x,0f,raycastHit.point.z));
+                StartCoroutine(PlayerAttack());
+            }
+        }
+
+        #endregion
+
+        #region Health
+
         healthBar.value = Stats.Health * 0.01f;
         //playerStats.HealthRegeneration();
-        
+
         //Test taking damage
         if (Input.GetKeyDown(KeyCode.K))
         {
@@ -83,37 +104,49 @@ public class PlayerController : MonoBehaviour
             //Handle death
             Debug.Log("Player Dead!");
         }
+
+        #endregion
     }
 
-    /// <summary>
+    #region Methods
+
+     /// <summary>
     /// Handle Movement
     /// </summary>
     private void Move()
     {
-        _movementInput = new Vector3(Input.GetAxis("Horizontal"),0f,Input.GetAxis("Vertical"));
-        
+        _movementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
         if (_movementInput.magnitude > 0)
         {
-            _myAnimator.SetBool(Running,true);
+            _myAnimator.SetBool(Running, true);
             RotateCharacter();
             var moveVector = transform.TransformDirection(Vector3.forward) * Stats.MoveSpeed;
-            _myRigidbody.velocity = new Vector3(moveVector.x,_myRigidbody.velocity.y,moveVector.z);
+            _myRigidbody.velocity = new Vector3(moveVector.x, _myRigidbody.velocity.y, moveVector.z);
         }
-        
-        if(_myRigidbody.velocity.magnitude == 0)
+
+        if (_myRigidbody.velocity.magnitude == 0)
         {
             //bug happens when going the contrary way of what was previously going
-            
-            _myAnimator.SetBool(Running,false);
+
+            _myAnimator.SetBool(Running, false);
         }
     }
 
+    /// <summary>
+    /// Handles character rotation
+    /// </summary>
     private void RotateCharacter()
     {
         var targetAngle = Mathf.Atan2(_movementInput.x, _movementInput.z) * Mathf.Rad2Deg;
-        transform.rotation =  Quaternion.Slerp(transform.rotation,Quaternion.Euler(0.0f,targetAngle,0.0f),SmoothTime);
+        transform.rotation =
+            Quaternion.Slerp(transform.rotation, Quaternion.Euler(0.0f, targetAngle, 0.0f), SmoothTime);
     }
 
+    /// <summary>
+    /// Handles dodging
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator HandleDodging()
     {
         _isDodging = true;
@@ -123,7 +156,7 @@ public class PlayerController : MonoBehaviour
             _myAnimator.SetTrigger(Dodging);
             var playerRollSpeed = Stats.MoveSpeed / 2;
             var dir = transform.forward * (playerRollSpeed) + Vector3.up * _myRigidbody.velocity.y;
-            _myRigidbody.AddForce(dir,ForceMode.Impulse);
+            _myRigidbody.AddForce(dir, ForceMode.Impulse);
             yield return null;
         }
 
@@ -131,37 +164,50 @@ public class PlayerController : MonoBehaviour
         {
             timer += Time.deltaTime;
         }
-        
+
         yield return new WaitForSeconds(_dodgeTimer);
         _isDodging = false;
     }
 
+    /// <summary>
+    /// Handles Attacking
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator PlayerAttack()
     {
         float timer = 0;
+
         if (timer < _attackTimer)
         {
             _combo++;
-            _myAnimator.SetInteger(Combo,_combo);
+            _myAnimator.SetInteger(Combo, _combo);
         }
 
         while (timer < _dodgeTimer)
         {
             timer += Time.deltaTime;
         }
-        
+
         yield return new WaitForSeconds(_attackTimer);
         _combo = 0;
-        _myAnimator.SetInteger(Combo,_combo);
+        _myAnimator.SetInteger(Combo, _combo);
     }
 
+    /// <summary>
+    /// Activates collider on sword
+    /// </summary>
     private void ActivateSword()
     {
         swordScript.ActivateCollider();
     }
 
+    /// <summary>
+    /// Deactivates collider on sword
+    /// </summary>
     private void DeactivateSword()
     {
         swordScript.DeactivateCollider();
     }
+
+    #endregion
 }
