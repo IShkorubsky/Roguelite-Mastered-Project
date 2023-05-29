c3386e173f444bc845f5ffd3738d1d233325eabb
IShkorubsky <illyashkorubsky@hotmail.com>
2023-05-29 22:20:05 +0100

    changes to ranged attack

--- Roguelite Mastered Project/Assets/Scripts/Player/PlayerAttack.cs
+++ Roguelite Mastered Project/Assets/Scripts/Player/PlayerAttack.cs
@@ -3,13 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 using UnityEngine.UI;
 
 public class PlayerAttack : PlayerAnimator
 {
     [SerializeField] private Transform arrowSpawnPosition;
     [SerializeField] private Collider swordCollider;
-    
+
     private void OnEnable()
     {
         arrowSpawnPosition = transform.GetChild(1).GetComponent<Transform>();
@@ -18,20 +19,23 @@ public class PlayerAttack : PlayerAnimator
     public void OnLeftMouseClick(InputAction.CallbackContext context)
     {
         IsAttackingMelee = context.ReadValueAsButton();
-        SetAnimatorBool(IsAttackingHash,IsAttackingMelee);
+        SetAnimatorBool(IsAttackingHash, IsAttackingMelee);
     }
 
     public void OnRightMouseClick(InputAction.CallbackContext context)
     {
-        IsAttackingRanged = context.ReadValueAsButton();
-        SetAnimatorBool(IsAttackingHash,IsAttackingRanged);
+        if (context.interaction is PressInteraction)
+        {
+            IsAttackingRanged = context.action.triggered;
+            SetAnimatorBool(IsAttackingHash, IsAttackingRanged);
+        }
     }
-    
+
     public void ActivateSwordCollider()
     {
         swordCollider.gameObject.GetComponent<Collider>().enabled = true;
     }
-    
+
     public void DeactivateSwordCollider()
     {
         swordCollider.gameObject.GetComponent<Collider>().enabled = false;
@@ -49,13 +53,15 @@ public class PlayerAttack : PlayerAnimator
     /// Handles Ranged Attacking
     /// </summary>
     /// <returns></returns>
-    private void RangedPlayerAttack()
+    private IEnumerator RangedPlayerAttack()
     {
         var bullet = Pool.Instance.Get("Bullet");
         bullet.transform.position = arrowSpawnPosition.transform.position;
         bullet.GetComponent<Rigidbody>().velocity = Vector3.zero;
         bullet.SetActive(true);
-        bullet.GetComponent<Rigidbody>().AddForce(arrowSpawnPosition.forward * GameManager.Instance.ChosenClass.RangedAttackSpeed,
+        bullet.GetComponent<Rigidbody>().AddForce(
+            arrowSpawnPosition.forward * GameManager.Instance.ChosenClass.RangedAttackSpeed,
             ForceMode.VelocityChange);
+        yield return new WaitForSeconds(2f);
     }
 }
